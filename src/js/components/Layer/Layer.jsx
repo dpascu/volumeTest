ec013567c0121e5dbce6bfc766f48775ab816bab
Daniel Pascual <daniel.pascual@dether.io>
2018-11-02 10:58:16 +0100

    [UPD] layer position

--- src/js/components/Layer/Layer.jsx
+++ src/js/components/Layer/Layer.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Layer = ({ children }) => (
-  <div className="layer">
+import LayerPositions from '../../constants/LayerPositions';
+
+/**
+ * Information layer over the video
+ * @param {Object} props
+ */
+const Layer = ({ children, position }) => (
+  <div className={`layer ${position}`}>
     {children}
   </div>
 );
 
 Layer.propTypes = {
   children: PropTypes.node.isRequired,
+  position: PropTypes.PropTypes.oneOf(LayerPositions),
+};
+
+Layer.defaultProps = {
+  position: 'left-up',
 };
 
 export default Layer;
